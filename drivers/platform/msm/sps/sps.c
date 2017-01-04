06018e139d1cebbd473d5e2d5256baf22ddf8c4e
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-01-04 16:51:53 +0100

    soc: qcom: stop spam when IPC_LOGGING is disabled
    Change-Id: 5f91ac2b1b2bafa2ec5955b949a2e678c486ef5f
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- drivers/platform/msm/sps/sps.c
+++ drivers/platform/msm/sps/sps.c
@@ -2200,6 +2200,7 @@ int sps_register_bam_device(const struct sps_bam_props *bam_props,
 
 	snprintf(bam_name, sizeof(bam_name), "sps_bam_%pa_0",
 					&bam->props.phys_addr);
+#ifdef CONFIG_IPC_LOGGING
 	bam->ipc_log0 = ipc_log_context_create(SPS_IPC_LOGPAGES,
 							bam_name, 0);
 	if (!bam->ipc_log0)
@@ -2221,7 +2222,6 @@ int sps_register_bam_device(const struct sps_bam_props *bam_props,
 	if (!bam->ipc_log2)
 		SPS_ERR(sps, "%s : unable to create IPC Logging 2 for bam %pa",
 					__func__, &bam->props.phys_addr);
-
 	snprintf(bam_name, sizeof(bam_name), "sps_bam_%pa_3",
 					&bam->props.phys_addr);
 	bam->ipc_log3 = ipc_log_context_create(SPS_IPC_LOGPAGES,
@@ -2237,6 +2237,7 @@ int sps_register_bam_device(const struct sps_bam_props *bam_props,
 	if (!bam->ipc_log4)
 		SPS_ERR(sps, "%s : unable to create IPC Logging 4 for bam %pa",
 					__func__, &bam->props.phys_addr);
+#endif
 
 	if (bam_props->ipc_loglevel)
 		bam->ipc_loglevel = bam_props->ipc_loglevel;
