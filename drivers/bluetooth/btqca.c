7c42fe825abda9ab1eb410172e57a63d1e351613
Marcel Holtmann <marcel@holtmann.org>
2017-10-30 10:42:59 +0100

    Bluetooth: Use bt_dev_err and bt_dev_info when possible
    In case of using BT_ERR and BT_INFO, convert to bt_dev_err and
    bt_dev_info when possible. This allows for controller specific
    reporting.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Change-Id: I41dbf737a41333d54a3f4b61c76eac9fa43c70b2
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    (cherry picked from commit 2064ee332e4c1b7495cf68b84355c213d8fe71fd)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- drivers/bluetooth/btqca.c
+++ drivers/bluetooth/btqca.c
@@ -292,7 +292,7 @@ static int rome_download_firmware(struct hci_dev *hdev,
 	struct tlv_type_hdr *tlv;
 	int ret;
 
-	BT_INFO("%s: ROME Downloading file: %s", hdev->name, config->fwname);
+	bt_dev_info(hdev, "ROME Downloading %s", config->fwname);
 	ret = request_firmware(&fw, config->fwname, &hdev->dev);
 
 	if (ret || !fw || !fw->data || fw->size <= 0) {
@@ -391,7 +391,7 @@ int qca_uart_setup_rome(struct hci_dev *hdev, uint8_t baudrate)
 		return err;
 	}
 
-	BT_INFO("%s: ROME controller version 0x%08x", hdev->name, rome_ver);
+	bt_dev_info(hdev, "ROME controller version 0x%08x", rome_ver);
 
 	/* Download rampatch file */
 	config.type = TLV_TYPE_PATCH;
@@ -423,7 +423,7 @@ int qca_uart_setup_rome(struct hci_dev *hdev, uint8_t baudrate)
 		return err;
 	}
 
-	BT_INFO("%s: ROME setup on UART is completed", hdev->name);
+	bt_dev_info(hdev, "ROME setup on UART is completed");
 
 	return 0;
 }
