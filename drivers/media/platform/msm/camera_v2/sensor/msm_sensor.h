4fa42fa6e1e50026f1a6d87c16597b7abf14c573
Hriday Sharma <hridaysharma42@gmail.com>
2018-03-01 21:37:59 +0000

    Revert "msm: camera: Bypass redundant creation of video node in stereo"
    This reverts commit 363d7dc52624689c6bdbfdeb3d92ceaf53c4413a.
    
    * Causes green tint effect in cam
    
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
@@ -89,7 +89,6 @@ struct msm_sensor_ctrl_t {
 	uint32_t set_mclk_23880000;
 	uint8_t is_csid_tg_mode;
 	uint32_t is_secure;
-	uint8_t bypass_video_node_creation;
 	struct msm_sensor_ctrl_t *peer_sensor_ctrl;
 };
 
