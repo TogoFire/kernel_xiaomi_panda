25ee61682bae77b1ca5a7f9b99d9dc6173da7c24
Josh Poimboeuf <jpoimboe@redhat.com>
2018-03-19 13:18:57 -0500

    jump_label: Disable jump labels in __exit code
    With the following commit:
    
      333522447063 ("jump_label: Explicitly disable jump labels in __init code")
    
    ... we explicitly disabled jump labels in __init code, so they could be
    detected and not warned about in the following commit:
    
      dc1dd184c2f0 ("jump_label: Warn on failed jump_label patching attempt")
    
    In-kernel __exit code has the same issue.  It's never used, so it's
    freed along with the rest of initmem.  But jump label entries in __exit
    code aren't explicitly disabled, so we get the following warning when
    enabling pr_debug() in __exit code:
    
      can't patch jump_label at dmi_sysfs_exit+0x0/0x2d
      WARNING: CPU: 0 PID: 22572 at kernel/jump_label.c:376 __jump_label_update+0x9d/0xb0
    
    Fix the warning by disabling all jump labels in initmem (which includes
    both __init and __exit code).
    
    Reported-and-tested-by: Li Wang <liwang@redhat.com>
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Change-Id: Ie7fd4005b4ab6192584b6f195176d1232fd85ed0
    Cc: Borislav Petkov <bp@suse.de>
    Cc: Jason Baron <jbaron@akamai.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Fixes: dc1dd184c2f0 ("jump_label: Warn on failed jump_label patching attempt")
    Link: http://lkml.kernel.org/r/7121e6e595374f06616c505b6e690e275c0054d1.1521483452.git.jpoimboe@redhat.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- kernel/jump_label.c
+++ kernel/jump_label.c
@@ -385,16 +385,15 @@ void __init jump_label_init(void)
 	jump_label_unlock();
 }
 
-/* Disable any jump label entries in __init code */
-void __init jump_label_invalidate_init(void)
+/* Disable any jump label entries in __init/__exit code */
+void __init jump_label_invalidate_initmem(void)
 {
 	struct jump_entry *iter_start = __start___jump_table;
 	struct jump_entry *iter_stop = __stop___jump_table;
 	struct jump_entry *iter;
 
 	for (iter = iter_start; iter < iter_stop; iter++) {
-		if (iter->code >= (unsigned long)_sinittext &&
-		    iter->code < (unsigned long)_einittext)
+		if (init_section_contains((void *)(unsigned long)iter->code, 1))
 			iter->code = 0;
 	}
 }
