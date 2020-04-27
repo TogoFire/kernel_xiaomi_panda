4ace830c178340db604638a4be27f1e5bf8814f2
darkhz <kmachanwenw@gmail.com>
2020-04-27 12:07:29 +0530

    drivers: char: Add srandom
    (Taken from https://github.com/josenk/srandom)
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

---
+++ drivers/char/srandom_defs.h
@@ -0,0 +1,6 @@
+/*
+ * Prototypes to use externally.
+ */
+extern ssize_t sdevice_read(struct file *, char *, size_t, loff_t *);
+extern ssize_t sdevice_write(struct file *, const char *, size_t, loff_t *);
+
