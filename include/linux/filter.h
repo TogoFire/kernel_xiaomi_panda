0361662ffe644d8c1e19cb68e4e3bf9240c995bb
Arnd Bergmann <arnd@arndb.de>
2020-04-30 23:30:47 +0200

    UPSTREAM: bpf: Avoid gcc-10 stringop-overflow warning in struct bpf_prog
    gcc-10 warns about accesses to zero-length arrays:
    
    kernel/bpf/core.c: In function 'bpf_patch_insn_single':
    cc1: warning: writing 8 bytes into a region of size 0 [-Wstringop-overflow=]
    In file included from kernel/bpf/core.c:21:
    include/linux/filter.h:550:20: note: at offset 0 to object 'insnsi' with size 0 declared here
      550 |   struct bpf_insn  insnsi[0];
          |                    ^~~~~~
    
    In this case, we really want to have two flexible-array members,
    but that is not possible. Removing the union to make insnsi a
    flexible-array member while leaving insns as a zero-length array
    fixes the warning, as nothing writes to the other one in that way.
    
    This trick only works on linux-3.18 or higher, as older versions
    had additional members in the union.
    
    Fixes: 60a3b2253c41 ("net: bpf: make eBPF interpreter images read-only")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Change-Id: I6a7f2a9e59c3d76089088817207bb0aeea9fd95f
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://lore.kernel.org/bpf/20200430213101.135134-6-arnd@arndb.de
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -438,10 +438,8 @@ struct bpf_prog {
 	unsigned int		(*bpf_func)(const struct sk_buff *skb,
 					    const struct bpf_insn *filter);
 	/* Instructions for interpreter */
-	union {
-		struct sock_filter	insns[0];
-		struct bpf_insn		insnsi[0];
-	};
+	struct sock_filter	insns[0];
+	struct bpf_insn		insnsi[];
 };
 
 struct sk_filter {
