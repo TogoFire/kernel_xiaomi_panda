beec1f3da1de6c9597af69954b2cb766263da1c6
Albert I <kras@raphielgang.org>
2020-05-22 14:11:52 +0800

    sakura-daisy: Update default and thermal mitigation charging currents
    Default values brought up from 3.18 don't go fairly well with 4.9 as device
    could go and heat up beyond 45 degrees Celcius and eventually stop charging
    due to how bad thermal mitigation voltages were set.
    
    In drivers/power/supply/qcom/qpnp-smbcharger.c:
    Make sure it's set like this:
    static int smbchg_default_hvdcp_icl_ma = 2000;
    static int smbchg_default_dcp_icl_ma = 2000;
    
    As Xiaomi uses default value of 2A on stock. It's rated at 2A max, if open your phone, it says it in the label.
    
    This should help mitigating this issue.
    
    [ TogoFire: Adapt to sakura-daisy ]
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I74a76805893dc746ca94c6becd2ae840aa710028
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- drivers/power/supply/qcom/qpnp-smbcharger.c
+++ drivers/power/supply/qcom/qpnp-smbcharger.c
@@ -453,7 +453,7 @@ module_param_named(
 	int, 00600
 );
 
-static int smbchg_default_hvdcp_icl_ma = 2500;
+static int smbchg_default_hvdcp_icl_ma = 2000;
 module_param_named(
 	default_hvdcp_icl_ma, smbchg_default_hvdcp_icl_ma,
 	int, 00600
