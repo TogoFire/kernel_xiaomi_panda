c19adf8b1f97b6a2f61fc7b446432ea5db485025
Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>
2020-09-16 12:11:53 -0400

    kernel: update import genpatches gentoo-fixes
    from https://anongit.gentoo.org/git/proj/linux-patches.git
    
    Gentoo Patches For Linux Kernel 4.9
    Tom Wijsman and Mike Pagano are the primary maintainers.
    
    1500_XATTR_USER_PREFIX.patch
    1510_fs-enable-link-security-restrictions-by-default.patch
    1520_security-apparmor-Use-POSIX-compatible-printf.patch
    1701_ia64_fix_ptrace.patch
    2000_BT-Check-key-sizes-only-if-Secure-Simple-Pairing-enabled.patch
    2300_enable-poweroff-on-Mac-Pro-11.patch
    2900_dev-root-proc-mount-fix.patch
    4400_alpha-sysctl-uac.patch
    5001_block-cgroups-kconfig-build-bits-for-BFQ-v7r11-4.9.patch
    5002_block-introduce-the-BFQ-v7r11-I-O-sched-for-4.9.patch
    5003_block-bfq-add-Early-Queue-Merge-EQM-to-BFQ-v7r11-for-4.9.patch
    5004_Turn-BFQ-v7r11-into-BFQ-v8r7-for-4.9.0.patch
    5010_enable-additional-cpu-optimizations-for-gcc.patch
    
    
    
    
    
    
    
    Change-Id: I6fb90b3431b5bc50b9c5e32d45bf3f8fd3aa365a

--- mm/shmem.c
+++ mm/shmem.c
@@ -3200,6 +3200,14 @@ static int shmem_xattr_handler_set(const struct xattr_handler *handler,
 	struct shmem_inode_info *info = SHMEM_I(inode);
 
 	name = xattr_full_name(handler, name);
+
+	if (!strncmp(name, XATTR_USER_PREFIX, XATTR_USER_PREFIX_LEN)) {
+		if (strcmp(name, XATTR_NAME_PAX_FLAGS))
+			return -EOPNOTSUPP;
+		if (size > 8)
+			return -EINVAL;
+	}
+
 	return simple_xattr_set(&info->xattrs, name, value, size, flags);
 }
 
@@ -3215,6 +3223,12 @@ static const struct xattr_handler shmem_trusted_xattr_handler = {
 	.set = shmem_xattr_handler_set,
 };
 
+static const struct xattr_handler shmem_user_xattr_handler = {
+	.prefix = XATTR_USER_PREFIX,
+	.get = shmem_xattr_handler_get,
+	.set = shmem_xattr_handler_set,
+};
+
 static const struct xattr_handler *shmem_xattr_handlers[] = {
 #ifdef CONFIG_TMPFS_POSIX_ACL
 	&posix_acl_access_xattr_handler,
@@ -3222,6 +3236,7 @@ static const struct xattr_handler *shmem_xattr_handlers[] = {
 #endif
 	&shmem_security_xattr_handler,
 	&shmem_trusted_xattr_handler,
+	&shmem_user_xattr_handler,
 	NULL
 };
 
