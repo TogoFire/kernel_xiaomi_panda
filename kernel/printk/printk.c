87fbe6757b6405bcbfad81a702e3b58d1334a177
penglezos <panagiotisegl@gmail.com>
2021-02-20 04:31:54 +0000

    kernel: printk: Ignore healthd and other charging spam logs.

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -770,6 +770,8 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			endp++;
 			len -= endp - line;
 			line = endp;
+			if (strstr(line, "healthd") || strstr(line, "cacert") || !strcmp(line, "CP: Couldn't"))
+			goto free;
 		}
 		}
 	}
@@ -782,6 +784,7 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 	printk_emit(facility, level, NULL, 0, "%s", line);
 ignore:
 	kfree(buf);
+	free:
 	return ret;
 }
 
