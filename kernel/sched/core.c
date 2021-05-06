eeff7e3d3a46a0b3f766a8b59b64bf58bc5d70f4
Sultan Alsawaf <sultan@kerneltoast.com>
2021-05-06 00:10:36 -0700

    sched: Resolve sched_feat() at compile time to improve code optimization
    Keeping the compiler in the dark about which sched_feat() toggles are
    set reduces its ability to optimize scheduler code, which results in a
    measurably significant performance loss since the affected code is very
    hot. Turning sched_feat() definitions into macros that the compiler can
    resolve results in smaller and faster code.
    
    The sched_feat() definitions are converted to macros with this sed:
    sed -i -E \
    	-e 's/SCHED_FEAT\((.*),(.*)\)/#define SCHED_FEAT_\1\2/' \
    	-e 's/true/1/' \
    	-e 's/false/0/' \
    	kernel/sched/features.h
    
    The sched_feat() macros are named SCHED_FEAT_*. With this done, we can
    now optimize try-to-wake-up code when SCHED_FEAT_TTWU_QUEUE is disabled,
    which results in a CPU usage reduction of a few percent inside
    try_to_wake_up().
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: I0ac75a10a67c8c277c84d003221dda7a71a12b51
    [ Tashar02: Adapt to k4.19 ]
    Co-authored-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    [ TogoFire: Adapt to k4.9 ]
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -89,19 +89,6 @@ void update_rq_clock(struct rq *rq)
 }
 
 /*
- * Debugging: various feature bits
- */
-
-#define SCHED_FEAT(name, enabled)	\
-	(1UL << __SCHED_FEAT_##name) * enabled |
-
-const_debug unsigned int sysctl_sched_features =
-#include "features.h"
-	0;
-
-#undef SCHED_FEAT
-
-/*
  * Number of tasks to iterate in a single balance run.
  * Limited because this is done with IRQs disabled.
  */
@@ -1797,6 +1784,7 @@ static int ttwu_remote(struct task_struct *p, int wake_flags)
 }
 
 #ifdef CONFIG_SMP
+#if SCHED_FEAT_TTWU_QUEUE
 void sched_ttwu_pending(void)
 {
 	struct rq *rq = this_rq();
@@ -1826,6 +1814,7 @@ void sched_ttwu_pending(void)
 	rq_unpin_lock(rq, &rf);
 	raw_spin_unlock_irqrestore(&rq->lock, flags);
 }
+#endif
 
 void scheduler_ipi(void)
 {
@@ -1838,9 +1827,14 @@ void scheduler_ipi(void)
 	 */
 	preempt_fold_need_resched();
 
+#if SCHED_FEAT_TTWU_QUEUE
 	if (llist_empty(&this_rq()->wake_list) && !got_nohz_idle_kick() &&
 							!got_boost_kick())
 		return;
+#else
+	if (!got_nohz_idle_kick())
+		return;
+#endif
 
 	if (got_boost_kick()) {
 		struct rq *rq = cpu_rq(cpu);
@@ -1876,6 +1870,7 @@ void scheduler_ipi(void)
 	irq_exit();
 }
 
+#if SCHED_FEAT_TTWU_QUEUE
 static void ttwu_queue_remote(struct task_struct *p, int cpu, int wake_flags)
 {
 	struct rq *rq = cpu_rq(cpu);
@@ -1889,6 +1884,7 @@ static void ttwu_queue_remote(struct task_struct *p, int cpu, int wake_flags)
 			trace_sched_wake_idle_without_ipi(cpu);
 	}
 }
+#endif
 
 void wake_up_if_idle(int cpu)
 {
@@ -1929,13 +1925,14 @@ static void ttwu_queue(struct task_struct *p, int cpu, int wake_flags)
 	struct rq_flags rf;
 
 #if defined(CONFIG_SMP)
-	if (sched_feat(TTWU_QUEUE) &&
-			!idle_cpu(cpu) &&
-			!cpus_share_cache(smp_processor_id(), cpu)) {
+	#if SCHED_FEAT_TTWU_QUEUE
+		if ((!idle_cpu(cpu) &&
+			!cpus_share_cache(smp_processor_id(), cpu))) {
 		sched_clock_cpu(cpu); /* Sync clocks across CPUs */
 		ttwu_queue_remote(p, cpu, wake_flags);
 		return;
 	}
+#endif
 #endif
 
 	raw_spin_lock(&rq->lock);
@@ -4079,8 +4076,10 @@ int idle_cpu(int cpu)
 		return 0;
 
 #ifdef CONFIG_SMP
+#if SCHED_FEAT_TTWU_QUEUE
 	if (!llist_empty(&rq->wake_list))
 		return 0;
+#endif
 #endif
 
 	return 1;
