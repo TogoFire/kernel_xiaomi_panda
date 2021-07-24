d701ac5945a6c74b395aa676bb4e6df36a36dcdf
TogoFire <italomellopereira@gmail.com>
2021-07-24 17:14:18 -0300

    drivers: mmc: core: sdio_cis.c: Fix strlcpy usage
    Fix these warnings clang:
    
    ./drivers/mmc/core/sdio_cis.c:63:31: warning: size argument in 'strlcpy' call appears to be size of the source; expected the size of the destination [-Wstrlcpy-strlcat-size]
                    strlcpy(string, buf, strlen(buf) + 1);
    
    Change-Id: 63e6164d0e6cc40e780f91bd44ad017838c31442
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- drivers/mmc/core/sdio_cis.c
+++ drivers/mmc/core/sdio_cis.c
@@ -60,7 +60,7 @@ static int cistpl_vers_1(struct mmc_card *card, struct sdio_func *func,
 
 	for (i = 0; i < nr_strings; i++) {
 		buffer[i] = string;
-		strlcpy(string, buf, strlen(buf) + 1);
+		strscpy(string, buf, strlen(buf) + 1);
 		string += strlen(string) + 1;
 		buf += strlen(buf) + 1;
 	}
@@ -420,4 +420,3 @@ void sdio_free_func_cis(struct sdio_func *func)
 
 	func->tuples = NULL;
 }
-
