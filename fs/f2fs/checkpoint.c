99ed4dba25de20d5212af0f65a2b9383b02e3b51
TogoFire <italomellopereira@gmail.com>
2021-09-03 09:45:14 -0300

    Merge branch 'f2fs-stable-linux-4.9.y'
    From: https://github.com/jaegeuk/f2fs-stable/commits/linux-4.9.y
    
    Signed-off-by: TogoFire <italomellopereira@gmail.com>
    Change-Id: Ice64a6948de989d834a9197fdb6f0f710bc42aa8

--- fs/f2fs/checkpoint.c
+++ fs/f2fs/checkpoint.c
@@ -1170,10 +1170,12 @@ static int block_operations(struct f2fs_sb_info *sbi)
 		.nr_to_write = LONG_MAX,
 		.for_reclaim = 0,
 	};
-	struct blk_plug plug;
 	int err = 0, cnt = 0;
 
-	blk_start_plug(&plug);
+	/*
+	 * Let's flush inline_data in dirty node pages.
+	 */
+	f2fs_flush_inline_data(sbi);
 
 retry_flush_quotas:
 	f2fs_lock_all(sbi);
@@ -1202,7 +1204,7 @@ retry_flush_dents:
 		f2fs_unlock_all(sbi);
 		err = f2fs_sync_dirty_inodes(sbi, DIR_INODE);
 		if (err)
-			goto out;
+			return err;
 		cond_resched();
 		goto retry_flush_quotas;
 	}
@@ -1218,7 +1220,7 @@ retry_flush_dents:
 		f2fs_unlock_all(sbi);
 		err = f2fs_sync_inode_meta(sbi);
 		if (err)
-			goto out;
+			return err;
 		cond_resched();
 		goto retry_flush_quotas;
 	}
@@ -1234,7 +1236,7 @@ retry_flush_nodes:
 		if (err) {
 			up_write(&sbi->node_change);
 			f2fs_unlock_all(sbi);
-			goto out;
+			return err;
 		}
 		cond_resched();
 		goto retry_flush_nodes;
@@ -1246,8 +1248,6 @@ retry_flush_nodes:
 	 */
 	__prepare_cp_block(sbi);
 	up_write(&sbi->node_change);
-out:
-	blk_finish_plug(&plug);
 	return err;
 }
 
