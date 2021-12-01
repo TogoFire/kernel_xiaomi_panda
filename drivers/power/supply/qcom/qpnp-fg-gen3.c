e6869d94737b192faa879f88d171cbc3b9c66565
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2021-12-01 22:32:38 +0600

    qpnp-fg-gen3: Disable fg_adjust_timebase for MACH_XIAOMI_MSM8953
    For the longcheer platform, this always returns -ENODATA.
    So, there's no point in calling fg_adjust_timebase for Wayne.
    
    This fixes the timebase adjustment failure logspams.
    
    Link: https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet/commit/92563eaa6e2dc8e167c20f56fece76af0d0444fe
    Co-authored-by: Dede Dindin Qudsy <xtrymind@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Change-Id: If50974a4239f3835007be637c9495e5875557f5c
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -4716,6 +4716,7 @@ static int fg_memif_init(struct fg_chip *chip)
 	return fg_ima_init(chip);
 }
 
+#ifndef CONFIG_MACH_XIAOMI_MSM8953
 static int fg_adjust_timebase(struct fg_chip *chip)
 {
 	int rc = 0, die_temp;
@@ -4749,6 +4750,7 @@ static int fg_adjust_timebase(struct fg_chip *chip)
 
 	return 0;
 }
+#endif
 
 /* INTERRUPT HANDLERS STAY HERE */
 
@@ -4861,10 +4863,11 @@ static irqreturn_t fg_delta_batt_temp_irq_handler(int irq, void *data)
 	chip->health = prop.intval;
 
 	if (chip->last_batt_temp != batt_temp) {
+#ifndef CONFIG_MACH_XIAOMI_MSM8953
 		rc = fg_adjust_timebase(chip);
 		if (rc < 0)
 			pr_err("Error in adjusting timebase, rc=%d\n", rc);
-
+#endif
 		rc = fg_adjust_recharge_voltage(chip);
 		if (rc < 0)
 			pr_err("Error in adjusting recharge_voltage, rc=%d\n",
@@ -4938,10 +4941,11 @@ static irqreturn_t fg_delta_msoc_irq_handler(int irq, void *data)
 	if (rc < 0)
 		pr_err("Error in validating ESR, rc=%d\n", rc);
 
+#ifndef CONFIG_MACH_XIAOMI_MSM8953
 	rc = fg_adjust_timebase(chip);
 	if (rc < 0)
 		pr_err("Error in adjusting timebase, rc=%d\n", rc);
-
+#endif
 	if (batt_psy_initialized(chip))
 		power_supply_changed(chip->batt_psy);
 
