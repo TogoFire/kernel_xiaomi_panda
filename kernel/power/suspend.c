22b35ddfeb389888d26e520299b22d8a7f044dee
Cyber Knight <cyberknight755@gmail.com>
2022-01-10 20:33:36 +0800

    kernel/power: Reduce verbosity of logging
    Silences:
    [41385.565295] Restarting tasks ...
    [41385.579844] Resume caused by IRQ 178, WLAN_CE_2
    [41385.579855] PM: PM: suspend exit 2022-01-10 10:54:26.808561566 UTC
    [41385.579857] PM: suspend exit
    [41385.680504] PM: PM: suspend entry 2022-01-10 10:54:26.909195420 UTC
    [41385.680519] PM: suspend entry (deep)
    [41385.704201] Freezing user space processes ...
    [41385.716293] Freezing remaining freezable tasks ...
    
    Link: https://github.com/StatiXOS/android_kernel_xiaomi_sunny/commit/5d5e035e6ddc4a92c70a514e1a66904773540a6a
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Change-Id: I0353a92ccaedff1182069b23180e40ae6c953b3a
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -271,7 +271,7 @@ static int suspend_test(int level)
 {
 #ifdef CONFIG_PM_DEBUG
 	if (pm_test_level == level) {
-		pr_info("suspend debug: Waiting for %d second(s).\n",
+		pr_debug("suspend debug: Waiting for %d second(s).\n",
 				pm_test_delay);
 		mdelay(pm_test_delay * 1000);
 		return 1;
