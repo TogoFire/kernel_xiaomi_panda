f7bf454432a9b65f5278655f3870dda430b82e7a
Uros Bizjak <ubizjak@gmail.com>
2022-05-18 20:49:53 +0200

    BACKPORT: sched/clock: Use try_cmpxchg64 in sched_clock_{local,remote}
    Use try_cmpxchg64 instead of cmpxchg64 (*ptr, old, new) != old in
    sched_clock_{local,remote}. x86 cmpxchg returns success in ZF flag,
    so this change saves a compare after cmpxchg (and related move
    instruction in front of cmpxchg).
    
    Signed-off-by: Uros Bizjak <ubizjak@gmail.com>
    Change-Id: I160cff2128e18ccc468c9b811c103fede78f7712
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20220518184953.3446778-1-ubizjak@gmail.com
    [ Tashar02: Backport to k4.19 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- kernel/sched/clock.c
+++ kernel/sched/clock.c
@@ -223,7 +223,7 @@ again:
 	clock = wrap_max(clock, min_clock);
 	clock = wrap_min(clock, max_clock);
 
-	if (cmpxchg64(&scd->clock, old_clock, clock) != old_clock)
+	if (!try_cmpxchg64(&scd->clock, &old_clock, clock))
 		goto again;
 
 	return clock;
@@ -285,7 +285,7 @@ again:
 		val = remote_clock;
 	}
 
-	if (cmpxchg64(ptr, old_val, val) != old_val)
+	if (!try_cmpxchg64(ptr, &old_val, val))
 		goto again;
 
 	return val;
