be6e82587b70e2628f4de810755d27a6ba4df4ca
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-07-05 14:05:53 +0600

    power: supply: Conditionally compile logging
    These spew quite the useless logs which we won't ever
    read and we don't them to be available on production builds.
    Disable them with an empty do-while loop if !DEBUG.
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Ia5c233ffd4f45911d54544d34d11296fdb496243

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -25,12 +25,18 @@
 #include "step-chg-jeita.h"
 #include "storm-watch.h"
 
+#ifdef DEBUG
 #define smblib_err(chg, fmt, ...)		\
 	pr_err("%s: %s: " fmt, chg->name,	\
 		__func__, ##__VA_ARGS__)	\
 
 #define smblib_dbg(chg, reason, fmt, ...)			\
 	do { } while (0)
+
+#else
+#define smblib_err(chg, fmt, ...) do {} while (0)
+#define smblib_dbg(chg, reason, fmt, ...) do {} while (0)
+#endif
 
 static bool is_secure(struct smb_charger *chg, int addr)
 {
@@ -3375,6 +3381,7 @@ int smblib_get_prop_slave_current_now(struct smb_charger *chg,
  * INTERRUPT HANDLERS *
  **********************/
 
+#ifdef DEBUG
 irqreturn_t smblib_handle_debug(int irq, void *data)
 {
 	struct smb_irq_data *irq_data = data;
@@ -3383,6 +3390,12 @@ irqreturn_t smblib_handle_debug(int irq, void *data)
 	smblib_dbg(chg, PR_INTERRUPT, "IRQ: %s\n", irq_data->name);
 	return IRQ_HANDLED;
 }
+#else
+inline irqreturn_t smblib_handle_debug(int irq, void *data)
+{
+	return IRQ_HANDLED;
+}
+#endif
 
 irqreturn_t smblib_handle_otg_overcurrent(int irq, void *data)
 {
