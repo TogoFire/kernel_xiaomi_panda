9687ab0ffb8ca531bef353faef4d7887626d7ea7
Togo 77 <italomellopereira@gmail.com>
2023-01-18 21:00:29 -0300

    uapi: fix restart device when playing videos
    Some roms need this, especially the current ones like Android Tiramisu, to be able to play any video without causing a restart.
    It also helps to play 1080p 60fps videos on youtube without lag.
    
    Change-Id: acd64137b7668ecb3e1c3592da7d846ccb0bcb95
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- include/uapi/media/msm_media_info.h
+++ include/uapi/media/msm_media_info.h
@@ -905,7 +905,7 @@ static inline unsigned int VENUS_EXTRADATA_SIZE(int width, int height)
 	 * In the future, calculate the size based on the w/h but just
 	 * hardcode it for now since 16K satisfies all current usecases.
 	 */
-	return 0; //16 * 1024;
+	return 16 * 1024;
 }
 
 /*
