6111814701262de8cdfd1570cedb2ef1f2125356
Togo 77 <togofire@mailfence.com>
2023-02-20 19:39:21 -0300

    Revert  "treewide: Add support docker"
    This fix bluetooth for caf rom like p404
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Ib9228be56013aec3e1b199609fcd6459476d4eca

--- block/blk-throttle.c
+++ block/blk-throttle.c
@@ -19,7 +19,7 @@ static int throtl_grp_quantum = 8;
 static int throtl_quantum = 32;
 
 /* Throttling is performed over 100ms slice and after that slice is renewed */
-static unsigned long throtl_slice = HZ/10;	/* 100 ms */
+static unsigned long throtl_slice = msecs_to_jiffies(100);	/* 100 ms */
 
 static struct blkcg_policy blkcg_policy_throtl;
 
