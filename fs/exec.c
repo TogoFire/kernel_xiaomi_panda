b578391ed99d8da9d30a42ba56b989f338098e26
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-05-01 17:01:51 +0800

    treewide: Guard KernelSU changes
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I2a15239e5aae12e9daa7f61ba3dd327aad6bc0d1

--- fs/exec.c
+++ fs/exec.c
@@ -1684,8 +1684,10 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+#ifdef CONFIG_KSU
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			       void *envp, int *flags);
+#endif
 
 static void android_service_blacklist(const char *name)
 {
@@ -1725,7 +1727,9 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
+#ifdef CONFIG_KSU
 	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2306,13 +2306,17 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 			    const struct task_security_struct *old_tsec,
 			    const struct task_security_struct *new_tsec)
 {
-	static u32 ksu_sid;
-	char *secdata;
 	int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);
 	int nosuid = !mnt_may_suid(bprm->file->f_path.mnt);
 	u32 av;
-	int rc, error;
-	u32 seclen;
+	int rc;
+
+#ifdef CONFIG_KSU
+        static u32 ksu_sid;
+        char *secdata;
+		int error;
+        u32 seclen;
+#endif
 
 	if (!nnp && !nosuid)
 		return 0; /* neither NNP nor nosuid */
@@ -2320,6 +2324,7 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 	if (new_tsec->sid == old_tsec->sid)
 		return 0; /* No change in credentials */
 
+#ifdef CONFIG_KSU
 	if (!ksu_sid)
 		security_secctx_to_secid("u:r:su:s0", strlen("u:r:su:s0"), &ksu_sid);
 
@@ -2330,6 +2335,7 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 		if (rc == 0 && new_tsec->sid == ksu_sid)
 			return 0;
 	}
+#endif
 
 	/*
 	 * If the policy enables the nnp_nosuid_transition policy capability,
