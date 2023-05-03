eeabfd338b48b94aee35fec0b1f23a62d4f3b3db
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-05-04 03:36:45 +0600

    fingerprint: fpc: Place __always_inline before function return types
    __always_inline is supposed to be placed before the return type of the
    functions. Current code is incorrect. So, fix this.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Change-Id: Ia2c5cbd61fc6825817498e2ed6dab81bef39c7f4
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- drivers/input/fingerprint/fpc/fpc1020_platform_tee.c
+++ drivers/input/fingerprint/fpc/fpc1020_platform_tee.c
@@ -559,7 +559,7 @@ static const struct attribute_group attribute_group = {
 	.attrs = attributes,
 };
 
-static irqreturn_t __always_inline fpc1020_irq_handler(int irq, void *handle)
+static __always_inline irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 {
 	struct fpc1020_data *fpc1020 = handle;
 
@@ -592,7 +592,7 @@ static inline int fpc1020_request_named_gpio(struct fpc1020_data *fpc1020,
 }
 
 /* modified by zhongshengbin for fingerprint D1S-634 begin 2018-03-04 */
-static int __always_inline fpc_fb_notif_callback(struct notifier_block *nb, unsigned long val,
+static __always_inline int fpc_fb_notif_callback(struct notifier_block *nb, unsigned long val,
 						 void *data)
 {
 	struct fpc1020_data *fpc1020 =
