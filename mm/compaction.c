f17a3f59a2c1fc7901a447a9a791a010bc701d23
TogoFire <togofire@mailfence.com>
2023-05-17 22:41:32 -0300

    Revert "mm: Compact memory mostly when fragmentation occurs"
    This reverts commit 4ff9992f833a959fa6c8fbf6edeed871477d6505.
    
    Link: https://github.com/Zazaaw/Zazaaw-ify_Kernel/commit/afb671009fa57db84e9d401dbe1814e3212eba32
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I2478957d96412cc3a45b26cb2b2fef9856c457ef

--- mm/compaction.c
+++ mm/compaction.c
@@ -1705,7 +1705,7 @@ static enum compact_result compact_zone_order(struct zone *zone, int order,
 	return ret;
 }
 
-int sysctl_extfrag_threshold = 800;
+int sysctl_extfrag_threshold = 500;
 
 /**
  * try_to_compact_pages - Direct compact to satisfy a high-order allocation
