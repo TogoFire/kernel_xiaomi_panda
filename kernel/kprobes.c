70d30208b0d0a42186141e806f22fad16e034fa4
TogoFire <togofire@mailfence.com>
2023-11-10 20:09:54 -0300

    Revert "kprobes: Prohibit probing on CFI preamble symbol"
    This reverts commit 0feb161e820911397133f49bb877a57b079746d7.
    
    Log:
    ld.lld: error: undefined symbol: str_has_prefix
    >>> referenced by kprobes.c
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I46d6a5e9f24bf5ee0e654bd0739bd0809fe961f8

--- kernel/kprobes.c
+++ kernel/kprobes.c
@@ -1496,17 +1496,6 @@ int __weak arch_check_ftrace_location(struct kprobe *p)
 	return 0;
 }
 
-static bool is_cfi_preamble_symbol(unsigned long addr)
-{
-	char symbuf[KSYM_NAME_LEN];
-
-	if (lookup_symbol_name(addr, symbuf))
-		return false;
-
-	return str_has_prefix("__cfi_", symbuf) ||
-		str_has_prefix("__pfx_", symbuf);
-}
-
 static int check_kprobe_address_safe(struct kprobe *p,
 				     struct module **probed_mod)
 {
@@ -1523,8 +1512,7 @@ static int check_kprobe_address_safe(struct kprobe *p,
 	    in_gate_area_no_mm((unsigned long) p->addr) ||
 	    within_kprobe_blacklist((unsigned long) p->addr) ||
 	    jump_label_text_reserved(p->addr, p->addr) ||
-	    find_bug((unsigned long)p->addr) ||
-	    is_cfi_preamble_symbol((unsigned long)p->addr)) {
+	    find_bug((unsigned long)p->addr)) {
 		ret = -EINVAL;
 		goto out;
 	}
